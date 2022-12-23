fd7410a8479506bc8eb1c7f9645e81c01119a489
Fred Aleixo <fredaleixo99@gmail.com>
2022-12-23 17:25:47 +0000

    Cleaned up unused files & uploaded datasets

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,15 +1,25 @@
 package org.example
+import org.apache.spark.sql.SparkSession
+
 
 /**
- * @author ${user.name}
+ * @author Frederico Aleixo
  */
 object App {
-  
+
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
+
+    val spark: SparkSession = SparkSession.builder
+      .appName("test")
+      .master("local[2]")
+      .getOrCreate()
+
+    val pathIn = "google-play-store-apps/googleplaystore.csv"
+    spark.read.csv(pathIn).show()
   }
 
 }
