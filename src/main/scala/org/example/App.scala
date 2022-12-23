17914df62a897751ea54d8cea2ae78613e42f8cf
Fred Aleixo <fredaleixo99@gmail.com>
2022-12-23 18:50:50 +0000

    Added Part 1 function

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,25 +1,32 @@
 package org.example
-import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
 /**
  * @author Frederico Aleixo
  */
 object App {
 
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+  def part1Func(spark: SparkSession): DataFrame = {
+    val pathIn = "google-play-store-apps/googleplaystore_user_reviews.csv"
+    val df = spark.read.option("header","true").csv(pathIn)
+
+    df.createOrReplaceTempView("data")
+
+    //Assuming that 'nan' values for Sentiment_Polarity are not included in the computation of AVG,
+    //to prevent skewing the data in the wrong direction
+    spark.sql("" +
+      "SELECT App, IFNULL(AVG(Sentiment_Polarity),0) as Average_Sentiment_Polarity " +
+      "FROM data " +
+      "WHERE Sentiment_Polarity IS NOT NULL AND Sentiment_Polarity != 'nan' " +
+      "GROUP BY App ")
+  }
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
 
     val spark: SparkSession = SparkSession.builder
       .appName("test")
       .master("local[2]")
       .getOrCreate()
 
-    val pathIn = "google-play-store-apps/googleplaystore.csv"
-    spark.read.csv(pathIn).show()
+    part1Func(spark).show()
   }
-
 }
